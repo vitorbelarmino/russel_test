d2ec96702a774e295a4172cf9cf4e12c14fab060
vitorbelarmino <vitor.belarmino@hotmail.com>
2025-02-26 16:28:49 -0300

    feat: add news products to the list

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -4,4 +4,22 @@ import mouse2 from "./mouse2.png";
 import logitech from "./logitech.png";
 import headset from "./headset.png";
 import soundbox from "./soundbox.png";
-export { bannerImage, mouse1, logitech, mouse2, headset, soundbox };
+import placaV1 from "./placaV1.png";
+import placaV2 from "./placaV2.png";
+import placaV3 from "./placaV3.png";
+import placaV4 from "./placaV4.png";
+import placaV5 from "./placaV5.png";
+
+export {
+  bannerImage,
+  mouse1,
+  logitech,
+  mouse2,
+  headset,
+  soundbox,
+  placaV1,
+  placaV2,
+  placaV3,
+  placaV4,
+  placaV5,
+};
--- src/app/mocks/products.ts
+++ src/app/mocks/products.ts
@@ -1,6 +1,17 @@
-import { headset, logitech, mouse1, mouse2, soundbox } from "../assets";
+import {
+  headset,
+  logitech,
+  mouse1,
+  mouse2,
+  placaV1,
+  placaV2,
+  placaV3,
+  placaV4,
+  placaV5,
+  soundbox,
+} from "../assets";
 
-const products = [
+const promotions = [
   {
     name: "Mouse Gamer Sem Fio Logitech G Pro X Superlight 2 com Lightspeed, 32000 DPI, Sensor Hero 2, com Bateria Recarregável, Preto",
     fullPrice: "R$ 1.629,48",
@@ -29,41 +40,44 @@ const products = [
     brandLogo: logitech,
     image: soundbox,
   },
-  // {
-  //   name: "Placa de Vídeo RX 6600 CLD 8G ASRock AMD Radeon, 8GB, GDDR6",
-  //   fullPrice: "R$ 2.267,47",
-  //   promotionPrice: "R$ 1.349,99",
-  //   brandLogo: "HARDWARE",
-  //   image: "link_da_imagem_5",
-  // },
-  // {
-  //   name: "Placa de Vídeo RX 6750XT Gaming XFX Graphics Card AMD Radeon, 12GB",
-  //   fullPrice: "R$ 4.229,99",
-  //   promotionPrice: "R$ 2.999,99",
-  //   brandLogo: "HARDWARE",
-  //   image: "link_da_imagem_6",
-  // },
-  // {
-  //   name: "Placa de Vídeo RTX 4060 AERO OC Gigabyte NVIDIA GeForce, 8GB GDDR6",
-  //   fullPrice: "R$ 3.599,99",
-  //   promotionPrice: "R$ 2.499,99",
-  //   brandLogo: "HARDWARE",
-  //   image: "link_da_imagem_7",
-  // },
-  // {
-  //   name: "Placa de Vídeo GTX 1650 D6 Ventus XS OCV3 MSI NVIDIA GeForce, 4GB GDDR6",
-  //   fullPrice: "R$ 1.047,36",
-  //   promotionPrice: "R$ 799,99",
-  //   brandLogo: "HARDWARE",
-  //   image: "link_da_imagem_8",
-  // },
-  // {
-  //   name: "Placa de Vídeo Gigabyte Radeon RX 6600 EAGLE AMD, 8GB GDDR6, Preto",
-  //   fullPrice: "R$ 2.477,94",
-  //   promotionPrice: "R$ 1.399,99",
-  //   brandLogo: "HARDWARE",
-  //   image: "link_da_imagem_9",
-  // },
 ];
 
-export { products };
+const products = [
+  {
+    name: "Placa de Vídeo RX 6600 CLD 8G ASRock AMD Radeon, 8GB, GDDR6",
+    fullPrice: "R$ 2.267,47",
+    promotionPrice: "R$ 1.349,99",
+    discountPix: 20,
+    image: placaV1,
+  },
+  {
+    name: "Placa de Vídeo RX 6750XT Gaming XFX Graphics Card AMD Radeon, 12GB GDDR6, Ray tracing, Fidelity FX",
+    fullPrice: "R$ 4.229,99",
+    promotionPrice: "R$ 2.999,99",
+    discountPix: 20,
+    image: placaV2,
+  },
+  {
+    name: "Placa de Vídeo RTX 4060 AERO OC Gigabyte NVIDIA GeForce, 8GB GDDR6, RGB, DLSS, Ray Tracing, Branco",
+    fullPrice: "R$ 3.599,99",
+    promotionPrice: "R$ 2.499,99",
+    discountPix: 20,
+    image: placaV3,
+  },
+  {
+    name: "Placa de Vídeo GTX 1650 D6 Ventus XS OCV3 MSI NVIDIA GeForce, 4GB GDDR6, G- Sync",
+    fullPrice: "R$ 1.047,36",
+    promotionPrice: "R$ 799,99",
+    discountPix: 20,
+    image: placaV4,
+  },
+  {
+    name: "Placa de Vídeo Gigabyte Radeon RX 6600 EAGLE AMD, 8GB GDDR6, Preto",
+    fullPrice: "R$ 2.477,94",
+    promotionPrice: "R$ 1.399,99",
+    discountPix: 20,
+    image: placaV5,
+  },
+];
+
+export { promotions, products };
