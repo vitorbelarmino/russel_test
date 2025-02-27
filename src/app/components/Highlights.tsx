d232d9d128e9c4474deb013294c9d2055967cf70
vitorbelarmino <vitor.belarmino@hotmail.com>
2025-02-27 20:03:12 -0300

    fix: responsive of highlights

--- src/app/components/Highlights.tsx
+++ src/app/components/Highlights.tsx
@@ -12,16 +12,21 @@ export default function Highlight() {
   };
   const highlight = products.filter((product) => product.highlight);
   return (
-    <div className="flex flex-col p-2 md:p-10 xl:px-32 gap-2 md:gap-10 justify-center bg-blackPrimary ">
-      <Image src={bannerImage} alt="highlights" />
+    <div className="flex flex-col p-4 md:p-10 xl:px-32 gap-2 md:gap-10 justify-center bg-blackPrimary">
+      <div
+        className="w-full aspect-[323/158] md:h-[366px] bg-cover bg-center 2xl:h-[450px]"
+        style={{ backgroundImage: `url(${bannerImage.src})` }}
+      ></div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-10 w-full ">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-center gap-4 w-full">
         {highlight.map((product, index) => (
           <div
             key={index}
-            className="flex flex-col justify-around bg-logitech-gradient gap-4 p-4 sm:p-6 w-full rounded-md cursor-default"
+            className="flex flex-col justify-around bg-logitech-gradient gap-4 p-4 sm:p-6 max-w-[310px] rounded-md cursor-default"
           >
-            <Image src={product.image} alt={product.name} />
+            <div>
+              <Image src={product.image} alt={product.name} />
+            </div>
             {product.brandLogo && <Image src={product.brandLogo} alt={product.name} />}
             <p className="text-logitechColor font-bold line-clamp-2 text-sm sm:line-clamp-3 sm:text-base">
               {product.name}
