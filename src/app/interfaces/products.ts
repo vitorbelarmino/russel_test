8b111eb4b4c0042820bfb8afb121c67949afd6e7
vitorbelarmino <vitor.belarmino@hotmail.com>
2025-02-27 00:06:57 -0300

    feat: merged promotions and products, created an interface to products

---
+++ src/app/interfaces/products.ts
@@ -0,0 +1,17 @@
+import { StaticImageData } from "next/image";
+
+export enum Stock {
+  InStock = "Em estoque",
+  OutOfStock = "Sem estoque",
+}
+export interface IProduct {
+  id: number;
+  name: string;
+  fullPrice: string;
+  promotionPrice: string;
+  discountPix?: number;
+  image: StaticImageData;
+  seller: string;
+  stock: Stock;
+  onSale: boolean;
+}
